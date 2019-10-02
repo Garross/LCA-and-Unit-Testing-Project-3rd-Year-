8ad13a79e3207b03b54c776ec45c9241cbf367d5
Garry Tiscovschi <47663911+Garross@users.noreply.github.com>
2019-10-03 00:26:05 +0100

    Introducing a binary tree to begin work on

---
+++ LCA.py
@@ -0,0 +1,8 @@
+from binarytree import tree #bst, Node
+#binary tree and Node packages have been installed
+
+values = [1,2,3,4,5,6,7]
+#    root = bst(
+ #   print(root)
+mytree = tree()
+print(mytree)
