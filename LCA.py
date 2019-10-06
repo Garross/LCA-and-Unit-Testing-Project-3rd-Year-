848d208539fe26494786585ab294b245be2e8c93
Garross <tiscovsg@tcd.ie>
2019-10-06 10:08:05 +0100

    Unit Testing opened

---
+++ LCA.py
@@ -0,0 +1,13 @@
+from binarytree import tree, build,bst
+#binary tree and Node packages have been installed
+
+# values = [1,2,3,4,5,6,7]
+# #    root = bst(
+#  #   print(root)
+# mytree = tree()
+# print(mytree)
+
+values = [1,2,3,4,5,6,7,9,10,11,12]
+
+root = bst(height=3, is_perfect=True)
+print(root)
