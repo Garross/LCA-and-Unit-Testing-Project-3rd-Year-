52de2f528f20ff47ffeb4e40980663bc9b13b4f0
Garross <tiscovsg@tcd.ie>
2019-10-17 22:56:57 +0100

    Found a new DAG and a potential DAG LCA by searching github for the key words DAG , LCA and Directed acyclic graph. The solution here seemed simpler than the previous one and so this was used to replace it.

--- DAG.py
+++ DAG.py
@@ -1,208 +1,129 @@
-from copy import copy, deepcopy
-from collections import deque
-import six_subset as six
+""" A Python Class
+A simple Python graph class, demonstrating the essential
+facts and functionalities of graphs.
 
-try:
-    from collections import OrderedDict
-except ImportError:
-    from ordereddict import OrderedDict
+FOUND ON GITHUB which led to website: https://www.nco.ncep.noaa.gov/pmb/codes/nwprod/nwm.v2.0.0/ush/resume_forecast/Graph.py
+"""
 
-
-class DAGValidationError(Exception):
-    pass
+import unittest
+import itertools
 
 
-class DAG(object):
-    """ Directed acyclic graph implementation. """
+class DAG_LCA:
 
-    def __init__(self):
-        """ Construct a new DAG with no nodes or edges. """
-        self.reset_graph()
+    def findLCA(dagDict, v1, v2):
 
-    def add_node(self, node_name, graph=None):
-        """ Add a node if it does not exist yet, or error out. """
-        if not graph:
-            graph = self.graph
-        if node_name in graph:
-            raise KeyError('node %s already exists' % node_name)
-        graph[node_name] = set()
+        solution = []
 
-    def add_node_if_not_exists(self, node_name, graph=None):
-        try:
-            self.add_node(node_name, graph=graph)
-        except KeyError:
-            pass
+        anc1 = [v1]
+        anc2 = [v2]
+        v1ancestors = getAncestors(v1, dagDict, anc1)
+        v2ancestors = getAncestors(v2, dagDict, anc2)
 
-    def delete_node(self, node_name, graph=None):
-        """ Deletes this node and all edges referencing it. """
-        if not graph:
-            graph = self.graph
-        if node_name not in graph:
-            raise KeyError('node %s does not exist' % node_name)
-        graph.pop(node_name)
+        for i in v1ancestors:
+            for i2 in v2ancestors:
+                if i == i2:
+                    solution.append(i)
 
-        for node, edges in six.iteritems(graph):
-            if node_name in edges:
-                edges.remove(node_name)
+        return solution
 
-    def delete_node_if_exists(self, node_name, graph=None):
-        try:
-            self.delete_node(node_name, graph=graph)
-        except KeyError:
-            pass
+    def getAncestors(v, dagDict, anc):
 
-    def add_edge(self, ind_node, dep_node, graph=None):
-        """ Add an edge (dependency) between the specified nodes. """
-        if not graph:
-            graph = self.graph
-        if ind_node not in graph or dep_node not in graph:
-            raise KeyError('one or more nodes do not exist in graph')
-        test_graph = deepcopy(graph)
-        test_graph[ind_node].add(dep_node)
-        is_valid, message = self.validate(test_graph)
-        if is_valid:
-            graph[ind_node].add(dep_node)
-        else:
-            raise DAGValidationError()
+        if prev(v, dagDict) == []:
+            return []
 
-    def delete_edge(self, ind_node, dep_node, graph=None):
-        """ Delete an edge from the graph. """
-        if not graph:
-            graph = self.graph
-        if dep_node not in graph.get(ind_node, []):
-            raise KeyError('this edge does not exist in graph')
-        graph[ind_node].remove(dep_node)
+        anc.append(prev(v, dagDict))
+        anc = list(itertools.chain(*anc))
 
-    def rename_edges(self, old_task_name, new_task_name, graph=None):
-        """ Change references to a task in existing edges. """
-        if not graph:
-            graph = self.graph
-        for node, edges in graph.items():
+        for i in prev(v, dagDict):
+            anc.append(getAncestors(i, dagDict, anc))
+            anc = list(itertools.chain(*anc))
 
-            if node == old_task_name:
-                graph[new_task_name] = copy(edges)
-                del graph[old_task_name]
+        return list(set(anc))
 
-            else:
-                if old_task_name in edges:
-                    edges.remove(old_task_name)
-                    edges.add(new_task_name)
+    def prev(v, dagDict):
 
-    def predecessors(self, node, graph=None):
-        """ Returns a list of all predecessors of the given node """
-        if graph is None:
-            graph = self.graph
-        return [key for key in graph if node in graph[key]]
+        prev = []
 
-    def downstream(self, node, graph=None):
-        """ Returns a list of all nodes this node has edges towards. """
-        if graph is None:
-            graph = self.graph
-        if node not in graph:
-            raise KeyError('node %s is not in graph' % node)
-        return list(graph[node])
+        for i in dagDict.keys():
+            if v in dagDict[i]:
+                prev.append(i)
 
-    def all_downstreams(self, node, graph=None):
-        """Returns a list of all nodes ultimately downstream
-        of the given node in the dependency graph, in
-        topological order."""
-        if graph is None:
-            graph = self.graph
-        nodes = [node]
-        nodes_seen = set()
-        i = 0
-        while i < len(nodes):
-            downstreams = self.downstream(nodes[i], graph)
-            for downstream_node in downstreams:
-                if downstream_node not in nodes_seen:
-                    nodes_seen.add(downstream_node)
-                    nodes.append(downstream_node)
-            i += 1
-        return list(
-            filter(
-                lambda node: node in nodes_seen,
-                self.topological_sort(graph=graph)
-            )
-        )
+        return prev
 
-    def all_leaves(self, graph=None):
-        """ Return a list of all leaves (nodes with no downstreams) """
-        if graph is None:
-            graph = self.graph
-        return [key for key in graph if not graph[key]]
 
-    def from_dict(self, graph_dict):
-        """ Reset the graph and build it from the passed dictionary.
-        The dictionary takes the form of {node_name: [directed edges]}
+# class to hold DAG objet
+# Adopted and modified from https://www.nco.ncep.noaa.gov/pmb/codes/nwprod/nwm.v2.0.0/ush/resume_forecast/Graph.py
+
+class DAG:
+
+    def __init__(self, graph_dict=None):
+        """ initializes a graph object
+            If no dictionary or None is given, an empty dictionary will be used
+        """
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        """ returns the vertices of a graph """
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        """ returns the edges of a graph """
+        return self.__generate_edges()
+
+    def add_vertex(self, vertex):
+        """ If the vertex "vertex" is not in
+            self.__graph_dict, a key "vertex" with an empty
+            list as a value is added to the dictionary.
+            Otherwise nothing has to be done.
+        """
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        """ assumes that edge is of type set, tuple or list;
+            between two vertices can be multiple edges!
+            Adds path from first given vertex to second given vertex
         """
 
-        self.reset_graph()
-        for new_node in six.iterkeys(graph_dict):
-            self.add_node(new_node)
-        for ind_node, dep_nodes in six.iteritems(graph_dict):
-            if not isinstance(dep_nodes, list):
-                raise TypeError('dict values must be lists')
-            for dep_node in dep_nodes:
-                self.add_edge(ind_node, dep_node)
+        if len(edge) != 2:
+            return
 
-    def reset_graph(self):
-        """ Restore the graph to an empty state. """
-        self.graph = OrderedDict()
+        vertex1, vertex2 = edge
 
-    def ind_nodes(self, graph=None):
-        """ Returns a list of all nodes in the graph with no dependencies. """
-        if graph is None:
-            graph = self.graph
-
-        dependent_nodes = set(
-            node for dependents in six.itervalues(graph) for node in dependents
-        )
-        return [node for node in graph.keys() if node not in dependent_nodes]
-
-    def validate(self, graph=None):
-        """ Returns (Boolean, message) of whether DAG is valid. """
-        graph = graph if graph is not None else self.graph
-        if len(self.ind_nodes(graph)) == 0:
-            return (False, 'no independent nodes detected')
-        try:
-            self.topological_sort(graph)
-        except ValueError:
-            return (False, 'failed topological sort')
-        return (True, 'valid')
-
-    def topological_sort(self, graph=None):
-        """ Returns a topological ordering of the DAG.
-        Raises an error if this is not possible (graph is not valid).
-        """
-        if graph is None:
-            graph = self.graph
-
-        in_degree = {}
-        for u in graph:
-            in_degree[u] = 0
-
-        for u in graph:
-            for v in graph[u]:
-                in_degree[v] += 1
-
-        queue = deque()
-        for u in in_degree:
-            if in_degree[u] == 0:
-                queue.appendleft(u)
-
-        l = []
-        while queue:
-            u = queue.pop()
-            l.append(u)
-            for v in graph[u]:
-                in_degree[v] -= 1
-                if in_degree[v] == 0:
-                    queue.appendleft(v)
-
-        if len(l) == len(graph):
-            return l
+        if vertex1 in self.__graph_dict and vertex2 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
         else:
-            raise ValueError('graph is not acyclic')
+            self.add_vertex(vertex2)
+            self.add_vertex(vertex1)
+            self.__graph_dict[vertex1].append(vertex2)
 
-    def size(self):
-        return len(self.graph)
+    def __generate_edges(self):
+        """ A static method generating the edges of the
+            graph "graph". Edges are represented as sets
+            with one (a loop back to the vertex) or two
+            vertices
+        """
+        edges = []
+        for vertex in self.__graph_dict:
+            for neighbour in self.__graph_dict[vertex]:
+                s = [vertex, neighbour]
+                if s not in edges:
+                    edges.append(s)
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self.__graph_dict:
+            res += str(k) + ", "
+        res += "edges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res
+
+    def graphDict(self):
+        return (self.__graph_dict)
+
+
